e97b14b1478eb76c99f83f77de7561b89762a084
Alejandro Jerónimo <infotecnoajf97@gmail.com>
2019-09-28 18:08:41 +0200

    Práctica 1 terminada.

--- practica01/objetos.cc
+++ practica01/objetos.cc
@@ -20,14 +20,15 @@ _puntos3D::_puntos3D()
 
 void _puntos3D::draw_puntos(float r, float g, float b, int grosor)
 {
-int i;
-glPointSize(grosor);
-glColor3f(r,g,b);
-glBegin(GL_POINTS);
-for (i=0;i<vertices.size();i++){
-	glVertex3fv((GLfloat *) &vertices[i]);
+
+	glPointSize(grosor);
+	glColor3f(r, g, b);
+	glBegin(GL_POINTS);
+	for (int i=0; i<vertices.size(); i++)
+	{
+		glVertex3fv((GLfloat *) &vertices[i]);
 	}
-glEnd();
+	glEnd();
 }
 
 
@@ -47,17 +48,16 @@ _triangulos3D::_triangulos3D()
 
 void _triangulos3D::draw_aristas(float r, float g, float b, int grosor)
 {
-int i;
-glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-glLineWidth(grosor);
-glColor3f(r,g,b);
-glBegin(GL_TRIANGLES);
-for (i=0;i<caras.size();i++){
-	glVertex3fv((GLfloat *) &vertices[caras[i]._0]);
-	glVertex3fv((GLfloat *) &vertices[caras[i]._1]);
-	glVertex3fv((GLfloat *) &vertices[caras[i]._2]);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glLineWidth(grosor);
+	glColor3f(r, g, b);
+	glBegin(GL_TRIANGLES);
+	for (int i=0; i<caras.size(); i++){
+		glVertex3fv((GLfloat *) &vertices[caras[i]._0]);
+		glVertex3fv((GLfloat *) &vertices[caras[i]._1]);
+		glVertex3fv((GLfloat *) &vertices[caras[i]._2]);
 	}
-glEnd();
+	glEnd();
 }
 
 
@@ -68,9 +68,16 @@ glEnd();
 
 void _triangulos3D::draw_solido(float r, float g, float b)
 {
-
-
-
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glColor3f(r, g, b);
+	glBegin(GL_TRIANGLES);
+	for (int i=0; i<caras.size(); i++)
+	{
+		glVertex3fv((GLfloat *) &vertices[caras[i]._0]);
+		glVertex3fv((GLfloat *) &vertices[caras[i]._1]);
+		glVertex3fv((GLfloat *) &vertices[caras[i]._2]);
+	}
+	glEnd();
 }
 
 //*************************************************************************
@@ -79,7 +86,21 @@ void _triangulos3D::draw_solido(float r, float g, float b)
 
 void _triangulos3D::draw_solido_ajedrez(float r1, float g1, float b1, float r2, float g2, float b2)
 {
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glColor3f(r1, g1, b1);
+	glBegin(GL_TRIANGLES);
+	for (int i=0; i<caras.size(); i++)
+	{
+		if(i % 2 == 0)
+			glColor3f(r1, g1, b1);
+		else
+			glColor3f(r2, g2, b2);
 
+		glVertex3fv((GLfloat *) &vertices[caras[i]._0]);
+		glVertex3fv((GLfloat *) &vertices[caras[i]._1]);
+		glVertex3fv((GLfloat *) &vertices[caras[i]._2]);
+	}
+	glEnd();
 }
 
 
@@ -90,10 +111,45 @@ void _triangulos3D::draw_solido_ajedrez(float r1, float g1, float b1, float r2, 
 
 _cubo::_cubo(float tam)
 {
-//vertices
+	// Vértices del cubo
+	vertices.resize(8);
 
-// triangulos
+	vertices[0].x = tam; vertices[0].y = tam; vertices[0].z = tam;
+	vertices[1].x = 0; vertices[1].y = tam; vertices[1].z = tam;
+	vertices[2].x = 0; vertices[2].y = 0; vertices[2].z = tam;
+	vertices[3].x = tam; vertices[3].y = 0; vertices[3].z = tam;
+	vertices[4].x = tam; vertices[4].y = 0; vertices[4].z = 0;
+	vertices[5].x = tam; vertices[5].y = tam; vertices[5].z = 0;
+	vertices[6].x = 0; vertices[6].y = tam; vertices[6].z = 0;
+	vertices[7].x = 0; vertices[7].y = 0; vertices[7].z = 0;
 
+	// Caras
+
+	caras.resize(12);
+
+	// Cara frontal
+	caras[0]._0 = 0; caras[0]._1 = 1; caras[0]._2 = 2;
+	caras[1]._0 = 0; caras[1]._1 = 2; caras[1]._2 = 3;
+
+	// Cara inferior
+	caras[2]._0 = 2; caras[2]._1 = 3; caras[2]._2 = 4;
+	caras[3]._0 = 2; caras[3]._1 = 4; caras[3]._2 = 7;
+
+	// Cara trasera
+	caras[4]._0 = 7; caras[4]._1 = 4; caras[4]._2 = 5;
+	caras[5]._0 = 7; caras[5]._1 = 6; caras[5]._2 = 5;
+
+	// Cara superior
+	caras[6]._0 = 1; caras[6]._1 = 0; caras[6]._2 = 5;
+	caras[7]._0 = 1; caras[7]._1 = 6; caras[7]._2 = 5;
+
+	// Cara lateral izquierda
+	caras[8]._0 = 1; caras[8]._1 = 7; caras[8]._2 = 6;
+	caras[9]._0 = 1; caras[9]._1 = 2; caras[9]._2 = 7;
+
+	// Cara lateral derecha
+	caras[10]._0 = 0; caras[10]._1 = 4; caras[10]._2 = 3;
+	caras[11]._0 = 0; caras[11]._1 = 4; caras[11]._2 = 5;
 }
 
 
